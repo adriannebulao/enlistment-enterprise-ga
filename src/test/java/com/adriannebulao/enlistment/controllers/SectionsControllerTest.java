53cf5cbc2ffcfc83f477e909c47468cae64d02da
adriannebulao <amcbulao@gmail.com>
2024-06-26 14:40:51 +0800

    feat: create section test: create section

--- src/test/java/com/adriannebulao/enlistment/controllers/SectionsControllerTest.java
+++ src/test/java/com/adriannebulao/enlistment/controllers/SectionsControllerTest.java
@@ -19,24 +19,54 @@ class SectionsControllerTest {
     @Test
     void createSection_save_new_section_to_repository() {
         // Given the controller, repositories & valid parameter arguments for creating a section
+        SubjectRepository subjectRepository = mock(SubjectRepository.class);
+        SectionRepository sectionRepository = mock(SectionRepository.class);
+        RoomRepository roomRepository = mock(RoomRepository.class);
+
         SectionsController sectionsController = new SectionsController();
+        sectionsController.setSubjectRepo(subjectRepository);
+        sectionsController.setSectionRepo(sectionRepository);
+        sectionsController.setRoomRepo(roomRepository);
+
         final String sectionId = DEFAULT_SECTION_ID;
         final String subjectId = DEFAULT_SUBJECT_ID;
-        final Days days = MTH;
-        final String start = "1:00PM";
-        final String end = "5:00PM";
-        final String roomName = "F612";
-        RedirectAttributes redirectAttributes = mock(RedirectAttributes.class);
-        // When the controller receives the arguments
-        String returnPath = sectionsController.createSection(sectionId, subjectId, days, start, end, roomName, redirectAttributes);
-        SectionRepository sectionRepository = mock(SectionRepository.class);
-        sectionsController.setSectionRepo(sectionRepository);
-        assertEquals("redirect:/enlist", returnPath);
-        // Then
-        // - it should retrieve the entities from the db, create a new section
-        // - save the section in the db
-        // - set a flash attribute called "sectionSuccessMessage" with the message "Successfully created new section " + sectionId
-        // - return the string value "redirect:sections" to redirect to the GET method
+        final String roomId = "Psych217";
 
+        Subject subject = mock(Subject.class);
+        Section section = mock(Section.class);
+        Room room = mock(Room.class);
+
+        when(subjectRepository.findById(subjectId)).thenReturn(Optional.of(subject));
+        when(roomRepository.findById(roomId)).thenReturn(Optional.of(room));
+        when(sectionRepository.save(any(Section.class))).thenReturn(section);
+
+        RedirectAttributes redirectAttrs = new RedirectAttributesModelMap();
+
+        // When the controller receives the arguments
+        String returnPath = sectionsController.createSection(
+                sectionId,
+                subjectId,
+                MTH,
+                LocalTime.of(8, 30).toString(),
+                LocalTime.of(10, 0).toString(),
+                "Psych217",
+                redirectAttrs
+        );
+
+        // Then
+        assertAll(
+                // - it should retrieve the entities from the db, create a new section
+                () -> verify(subjectRepository).findById(subjectId),
+                () -> verify(roomRepository).findById(roomId),
+
+                // - save the section in the db
+                () -> verify(sectionRepository).save(any(Section.class)),
+
+                // - set a flash attribute called "sectionSuccessMessage" with the message "Successfully created new section " + sectionId
+                () -> assertEquals("Successfully created new section " + sectionId, redirectAttrs.getFlashAttributes().get("sectionSuccessMessage")),
+
+                // - return the string value "redirect:sections" to redirect to the GET method
+                () -> assertEquals("redirect:sections", returnPath)
+        );
     }
 }
